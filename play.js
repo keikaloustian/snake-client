89236ec3e6e944702e64db12f6907b4a17869b44
Kei Kaloustian <kei.kaloustian@gmail.com>
2022-09-28 14:28:03 -0700

    Setup stdin

--- play.js
+++ play.js
@@ -1,3 +1,27 @@
 const connect = require('./client');
 
+// connect to server and begin playing
 connect();
+
+// callback for key press event listener
+const handleUserInput = function (data) {
+  // Ctrl+C to quit
+  if (data === '\u0003') {
+    process.exit();
+  }
+
+  
+};
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  stdin.on('data', handleUserInput);
+
+  return stdin;
+};
+
+setupInput();
